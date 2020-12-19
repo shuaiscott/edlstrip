cecbc2a62f53852b24b454b7a19702eef4c9b6be
Scott Carlson <shuaiscott@users.noreply.github.com>
2020-12-19 07:59:31 +0000

    Adding TDD unit tests

--- edlstrip.py
+++ edlstrip.py
@@ -1,37 +1,93 @@
-import ffmpeg
-import os
+import os, sys, logging
 import argparse
+import ffmpeg
 
 ###
 # Parse Args
 ###
-parser = argparse.ArgumentParser(description='Strips commercials off Channels DVR recordings using outputted EDL')
-parser.add_argument('video', type=str,
-                    help='video file to strip')
-parser.add_argument('edl', type=str,
-                    help='EDL file used to control stripping')
-parser.add_argument('--vcodec', dest='vcodec',
-                    help='the video codec to transcode file to')
-parser.add_argument('--acodec', dest='acodec',
-                    help='the audio codec to transcode file to')
-args = parser.parse_args()
 
-def to_timecode(seconds):
+
+## 
+# Helper functions
+##
+def parse_args(args):
+    """
+    Parses commandline arguments and provides help/interface info
+
+    Throws error if required file arguments don't exist on filesystem
+    """
+    parser = argparse.ArgumentParser(description='Strips commercials off Channels DVR recordings using outputted EDL')
+    parser.add_argument('video', type=str,
+                        help='video file to strip')
+    parser.add_argument('edl', type=str,
+                        help='EDL file used to control stripping')
+    parser.add_argument('--vcodec', dest='vcodec',
+                        help='the video codec to transcode file to')
+    parser.add_argument('--acodec', dest='acodec',
+                        help='the audio codec to transcode file to')
+    args = parser.parse_args()
+
+    # Check file existence
+    if (not os.path.exists(args.video)):
+        sys.exit(f"Error: Video file '{args.video}' doesn't exist")
+    if (not os.path.exists(args.edl)):
+        sys.exit(f"Error: EDL file '{args.edl}' doesn't exist")
+    return args
+        
+
+def to_timecode(input_seconds):
     """
     input: seconds
     from: https://stackoverflow.com/a/21520196
     """
-    return '{:02}:{:02}:{:02}'.format(seconds//3600, seconds%3600//60, seconds%60)
+    # Convert input to float ("Break in case of Strings")
+    seconds = float(input_seconds)
+
+    decimal = round(seconds % 1, 3) # round to 3 decimals to help with floating point errors
+    int_seconds = int(round(seconds - decimal))
+    undecimal = int(decimal * 1000)
+    return '{:02}:{:02}:{:02}.{:03}'.format(int_seconds//3600, int_seconds%3600//60, int_seconds%60, undecimal)
+
+def parse_edl(edlfile):
+    """
+    input edl file to parse
+    """
+    edl_item_list = []
+    with open(edlfile) as fp:
+        line = fp.readline()
+        cnt = 0
+        while line:
+            start, stop = line.split()[:2]
+            logging.debug(f"Split EDL line to {start},{stop}")
+            tc_start = to_timecode(start)
+            tc_stop = to_timecode(stop)
+            edl_tuple = (tc_start, tc_stop)
+            logging.debug(f"Created tuple: {edl_tuple}")
+            edl_item_list.append(edl_tuple)
+            line = fp.readline()
+            cnt += 1
+        logging.info(f"Read {cnt} lines from {edlfile}")
+    return edl_item_list
+##
+# Main Execution
+##
+if __name__ == '__main__':
+    # Parse Args
+    parser = parse_args(sys.argv[1:])
+
+    # Setup Logger
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
-# HALP, ffmpeg-python doesn't let you seek like:
-# ffmpeg -ss 00:01:00 -i input.mp4 -to 00:02:00 -c copy output.mp4
-# https://stackoverflow.com/a/42827058
-#
-# video = ffmpeg.input('CreuxDeVan.mpg')
-# trimmed = ffmpeg.trim(video, start=0, duration=1)
-# out = ffmpeg.output(trimmed, 'test.mpg', vcodec='copy', acodec='copy')
-# out.run()
+
+    # HALP, ffmpeg-python doesn't let you seek like:
+    # ffmpeg -ss 00:01:00 -i input.mp4 -to 00:02:00 -c copy output.mp4
+    # https://stackoverflow.com/a/42827058
+    #
+    # video = ffmpeg.input('CreuxDeVan.mpg')
+    # trimmed = ffmpeg.trim(video, start=0, duration=1)
+    # out = ffmpeg.output(trimmed, 'test.mpg', vcodec='copy', acodec='copy')
+    # out.run()
 
 
 
--- test_edlstrip.py
+++ test_edlstrip.py
@@ -1,12 +1,31 @@
 import pytest
-from edlstrip import to_timecode
+from unittest.mock import patch, mock_open
+from edlstrip import to_timecode, parse_edl
 
 @pytest.mark.parametrize("seconds, timecode", [
-    (3600, '01:00:00'),
-    (0, '00:00:00'),
-    (100, '00:01:40'),
-    (1000, '00:16:40'),
-    (36001, '10:00:01'),
+    (3600, '01:00:00.000'),
+    ('3600', '01:00:00.000'),
+    (0, '00:00:00.000'),
+    (100, '00:01:40.000'),
+    (1000, '00:16:40.000'),
+    (36001, '10:00:01.000'),
+    (3600.500, '01:00:00.500'),
+    (3600.2500, '01:00:00.250'),
 ])
 def test_to_timecode(seconds, timecode):
     assert to_timecode(seconds) == timecode
+
+
+
+@pytest.mark.parametrize("file_data, timecode_tuple_list", [
+    ("1 2", [("00:00:01.000", "00:00:02.000")]),
+    ("1 2\n1 2", [("00:00:01.000", "00:00:02.000"),("00:00:01.000", "00:00:02.000")]),
+    ("1 2\n3 4\n5 6\n7 8\n9 10\n11 12", [("00:00:01.000", "00:00:02.000"),("00:00:03.000", "00:00:04.000"),("00:00:05.000", "00:00:06.000"),("00:00:07.000", "00:00:08.000"),("00:00:09.000", "00:00:10.000"),("00:00:11.000", "00:00:12.000")]),
+    ("1 2.223", [("00:00:01.000", "00:00:02.223")]),
+    ("1 1.1111111111111", [("00:00:01.000", "00:00:01.111")]),
+])
+def test_parse_edl(file_data, timecode_tuple_list):
+    with patch('builtins.open', mock_open(read_data=file_data)):
+        edl_list = parse_edl('mocked params')
+        assert edl_list == timecode_tuple_list
+    
