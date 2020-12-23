c9119b88dbe51d0e53efec1ae290af7f24a23cc7
Scott Carlson <shuaiscott@gmail.com>
2020-12-22 22:49:42 -0700

    reducing python version requirement

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3',
     entry_points = {
         'console_scripts': ['edlstrip=edlstrip:main'],
     }
